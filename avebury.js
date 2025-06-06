28bac5b4570e65aa00cd6f17d08cf5c1a3ac68ff
Madeline Meltesen <157516209+madeline-meltesen@users.noreply.github.com>
2025-06-06 16:26:53 +0100

    Add files via upload

--- avebury.js
+++ avebury.js
@@ -22,6 +22,22 @@ var baseLayers = {
 // Create menu window for basemap control
 var layerControl = L.control.layers(baseLayers, {}, { collapsed: false }).addTo(mymap);
 
+// Create reset view button
+var ZoomOutControl = L.Control.extend({
+    options: {
+        position: 'topleft'
+    },
+    onAdd: function(map) {
+        var button = L.DomUtil.create('button', 'custom-button');
+            button.title = 'Reset view';
+            button.onclick = function() {
+                map.setView([51.4286, -1.8538], 17); 
+                };
+                return button;
+    }
+});
+new ZoomOutControl().addTo(mymap); 
+
 // Create locator (inset) map
 var miniMap = new L.Control.MiniMap(L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'), {
     toggleDisplay: true,
@@ -37,15 +53,34 @@ L.control.scale({
 }).addTo(mymap);
 
 
+
+// GeoJSON layer control
+var overlayLayers = {};
+
 // Add GeoJSONs to the map
-fetch('GeoJSONs/c01_AubreyPlanA.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: {
-        weight: 0,
-        fillColor: "black",
-        fillOpacity: 0.7
-      }
+function loadGeoJSON(url, name, color) {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            overlayLayers[name] = L.geoJSON(data, {
+                style: {
+                    weight: 0,
+                    fillColor: color,
+                    fillOpacity: 0.5
+                }
+            }).addTo(mymap);
+        });
+}
+
+// Initialise layer control
+Promise.all([
+    loadGeoJSON('GeoJSONs/c01_AubreyPlanA.geojson', "01: Aubrey Plan A", "black"),
+    loadGeoJSON('GeoJSONs/c02_StukeleyPlanE.geojson', "02: Stukeley Plan E", "red"),
+]).then(() => {
+    var overlayControl = L.control.layers(null, overlayLayers, {
+        collapsed: false,
+        position: 'topleft'
     }).addTo(mymap);
-  });
+    
+    overlayControl.getContainer().classList.add('geojson-overlay-control');
+})
